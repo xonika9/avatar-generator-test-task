522825aa294be0faba86e01a17f9bc915ad0e83e
xonika9 <xonika9@gmail.com>
2023-04-21 15:44:28 +0300

    add scripts to generate & download avatar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,130 @@
 <template>
   <div class="avatar" ref="avatar">
-    <img />
+    <img
+      class="avatar-image"
+      v-for="(part, index) in avatarParts"
+      :key="index"
+      :src="part.src"
+      :style="{
+        position: 'absolute',
+        top: part.top,
+        left: part.left,
+        transform: part.transform,
+      }"
+    />
   </div>
   <button class="button" @click="generateAvatar">Generate</button>
   <button class="button" @click="downloadAvatar">Download</button>
 </template>
 
-<style>
-.avatar {
-  position: relative;
-  overflow: hidden;
-  margin: 0 auto;
-  width: 440px;
-  height: 440px;
-  border-radius: 10px;
-  background-color: #f5f5f5;
-}
+<script>
+import { ref, reactive, onMounted } from 'vue';
+import { toPng } from 'dom-to-image-more';
+import { saveAs } from 'file-saver';
 
-.avatar-image {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
+export default {
+  setup() {
+    const avatar = ref(null);
+    const avatarParts = reactive([]);
 
-.button {
-  margin: 10px;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  background-color: #3498db;
-  color: white;
-  font-size: 18px;
-  cursor: pointer;
-}
+    const parts = [
+      {
+        type: 'background',
+        images: import.meta.globEager('./assets/avatar-parts/background/*.png'),
+        top: '0',
+        left: '0',
+      },
+      {
+        type: 'head',
+        images: import.meta.globEager('./assets/avatar-parts/head/*.png'),
+        top: '125px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'eyes',
+        images: import.meta.globEager('./assets/avatar-parts/eyes/*.png'),
+        top: '196px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'eyebrows',
+        images: import.meta.globEager('./assets/avatar-parts/eyebrows/*.png'),
+        top: '177px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'mouth',
+        images: import.meta.globEager('./assets/avatar-parts/mouth/*.png'),
+        top: '172px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'glasses',
+        images: import.meta.globEager('./assets/avatar-parts/glasses/*.png'),
+        top: '191px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'body',
+        images: import.meta.globEager('./assets/avatar-parts/body/*.png'),
+        top: '290px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'top',
+        images: import.meta.globEager('./assets/avatar-parts/top/*.png'),
+        top: '58px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      },
+      {
+        type: 'pet',
+        images: import.meta.globEager('./assets/avatar-parts/pet/*.png'),
+        top: '260px',
+        left: '-80px',
+        transform: 'rotate(-20deg)',
+      },
+    ];
 
-.button:hover {
-  background-color: #2980b9;
-}
-</style>
+    const getRandomImage = (images) => {
+      const keys = Object.keys(images);
+      const randomIndex = Math.floor(Math.random() * keys.length);
+      const imagePath = keys[randomIndex];
+      const image = images[imagePath];
+      return image && image.default ? image.default : '';
+    };
+
+    const generateAvatar = () => {
+      avatarParts.splice(0, avatarParts.length);
+
+      parts.forEach((part) => {
+        avatarParts.push({
+          src: getRandomImage(part.images),
+          top: part.top,
+          left: part.left,
+          transform: part.transform,
+        });
+      });
+    };
+
+    const downloadAvatar = async () => {
+      const dataUrl = await toPng(avatar.value);
+      saveAs(dataUrl, 'avatar.png');
+    };
+
+    onMounted(() => {
+      generateAvatar();
+    });
+
+    return { avatar, avatarParts, generateAvatar, downloadAvatar };
+  },
+};
+</script>
+
+<style src="./App.css" scoped></style>
