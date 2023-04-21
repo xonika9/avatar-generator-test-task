2b5116ebf95c80280f2bf51a39fcf2e2c3d2f9bf
xonika9 <xonika9@gmail.com>
2023-04-21 15:35:27 +0300

    add template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="avatar" ref="avatar">
+    <img />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button class="button" @click="generateAvatar">Generate</button>
+  <button class="button" @click="downloadAvatar">Download</button>
 </template>
 
 <style scoped>
